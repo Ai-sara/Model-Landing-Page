1161bb19ea7ff48e1d80d00bc10e2ff72ed2131b
Aisara <112186305+Ai-sara@users.noreply.github.com>
2022-12-23 00:15:07 +0600

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const navMenu = document.getElementById('nav-menu');
+const toggleMenu = document.getElementById('nav-toggle');
+const closeMenu = document.getElementById('nav-close');
+
+// SHOW
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+    navMenu.classList.remove('hidden')
+})
+// HIDDEN
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+    navMenu.classList.remove('hidden')
+})
